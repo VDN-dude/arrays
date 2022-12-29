6bf499f5cfe75995fcea2c7f5701a5323a589c39
Yuri-Vanderon <garanovich2@gmail.com>
2022-12-29 13:12:26 +0300

    added points 1 and 3

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int [] arr = {-45, -787, 676, 909, 76};
-//point 1
+        int [] arr = {-4, -787678, 6, 909, 76165};
+
+//point 1 (Min & Max numbers)
+
+        int min = 0;
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-
+            if (arr[i] < min){
+                 min = arr[i];
+            }
         }
+        System.out.println("Min number in the array : " + min);
 
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Min number in the array : " + max);
 
-//point 2
+//point 2 (Evens & Odds numbers)
 
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] % 2 == 0){
@@ -22,11 +34,42 @@ public class Main {
             }
         }
 
-//point 5
+//point 3 (Shortest & Longest numbers)
+
+        int shortest = 0;
+        int longest = 0;
+        int start = 0;
+        for (int i = 0; i < arr.length; i++) {
+            String stringLenght = String.valueOf(Math.abs(arr[i]));
+            int lenght = stringLenght.length();
+            if (lenght > start){
+                start = lenght;
+                longest = arr[i];
+            }
+        }
+        System.out.println("One of the Longests numbers : " + longest);
+
+        for (int i = 0; i < arr.length; i++) {
+            String stringLenght = String.valueOf(Math.abs(arr[i]));
+            int lenght = stringLenght.length();
+            if (lenght < start){
+                start = lenght;
+                shortest = arr[i];
+            }
+        }
+        System.out.println("One of the Shortest numbers : " + shortest);
+
+// point 4 (Display numbers that are in ascending order)
+
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+
+//point 5 (Palindromes)
 
         System.out.print("Palindromes : ");
         for (int i = 0; i < arr.length; i++) {
-            String snum = Integer.toString(Math.abs(arr[i]));
+            String snum = String.valueOf(Math.abs(arr[i]));
             StringBuilder lenght = new StringBuilder(snum);
             int l = Integer.parseInt(lenght.reverse().toString());
             int num = Integer.parseInt(snum);
